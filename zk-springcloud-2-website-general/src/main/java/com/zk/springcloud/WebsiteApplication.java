b58e2d0d1ca7bcf0f76b07f9e8991127b3fe971a
zk <Mrzhang@qq.com>
2017-09-28 19:42:29 +0800

    springcloud 服务消费者 非负载均衡

--- zk-springcloud-2-website-general/src/main/java/com/zk/springcloud/WebsiteApplication.java
+++ zk-springcloud-2-website-general/src/main/java/com/zk/springcloud/WebsiteApplication.java
@@ -4,6 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+/**
+ * 服务消费者 非负载均衡
+ * @author Administrator
+ *
+ */
 @SpringBootApplication
 @EnableEurekaClient
 public class WebsiteApplication {	
