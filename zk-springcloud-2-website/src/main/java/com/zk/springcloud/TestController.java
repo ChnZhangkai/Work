9cc11b6d742553efb93befecb45fb3c8e4647c50
zk <Mrzhang@qq.com>
2017-09-28 09:21:38 +0800

    springcloud 服务消费者 负载均衡

--- zk-springcloud-2-website/src/main/java/com/zk/springcloud/TestController.java
+++ zk-springcloud-2-website/src/main/java/com/zk/springcloud/TestController.java
@@ -1,7 +1,5 @@
 package com.zk.springcloud;
 
-import java.util.HashMap;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
