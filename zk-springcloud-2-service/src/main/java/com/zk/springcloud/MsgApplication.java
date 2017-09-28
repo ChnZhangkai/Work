978195b76b5532ddb0c8c7a99e949be0f4a3c010
zk <Mrzhang@qq.com>
2017-09-28 19:40:26 +0800

    springcloud 服务提供者1

--- zk-springcloud-2-service/src/main/java/com/zk/springcloud/MsgApplication.java
+++ zk-springcloud-2-service/src/main/java/com/zk/springcloud/MsgApplication.java
@@ -4,6 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+/**
+ * 服务提供者
+ * @author Administrator
+ *
+ */
 @SpringBootApplication
 @EnableEurekaClient
 public class MsgApplication {
